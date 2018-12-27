98fc4d67f0024351b544225d5758ac943d000c93
KimgyeyeonHome <rpdus25@cubm.co.kr>
2018-12-27 23:02:22 +0900

    'note입력후빈value가되게수정'

--- 2-week/homework/src/App.js
+++ 2-week/homework/src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import Writing from './Writing';
+import Note from './Note';
 
 class App extends Component {
-  render() {
+    constructor(props) {
+      super(props);
+      this.state = {
+        savedNotes: [
+        //편의를 위해 content 라는 key값을 줘보겠습니다.
+        // {} 안에는 key: value 형태로 값을 지정해 줄 수 있습니다.
+        {content: "default1"},
+        {content: "default2"},
+        {content: "default3"}
+      ]
+    }
+  }
+
+    handleSubmit = (userInput) => {
+        const savedNotes = this.state.savedNotes;
+        this.setState({
+            savedNotes: [
+                ...savedNotes,
+                //content 안에 userInput을 넣어야, content로 저장이 됩니다.
+                {content: userInput}
+            ]
+        })
+    }
+
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {/*수업시간에는 save로 했었는데, submit이 더 의미론적인(바로 알아보기 편한) 표현이므로 submit으로 모두 바꿨습니다.*/}
+
+        <Writing submit={this.handleSubmit} />
+        <div className='row'>
+          {/*실습시간에는 map 내부에 return을 넣어서 저장했습니다.*/}
+          {/*이는 함수를 설정할 때 (note) => {}형태로 만들었기 때문에 새로운 scope가 생겼고,*/}
+          {/*새로운 scope가 생겼을 경우에는 return을 통해 반환 값을 설정해 줘야 map이 작동합니다.*/}
+          {/*지금은 조금 어려울 수 있는 내용이니, 일단 {}형태로 함수를 선언하면 return이 필요하고*/}
+          {/*아닐 경우에는 안넣어도 된다고 이해하고 넘어갑시다.*/}
+          {this.state.savedNotes.map((note) => (
+            <Note content={note.content} />
+          ))}
+        </div>
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ 2-week/homework/src/Writing.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Writing extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: "",
+            userInputPlaceHolder: "텍스트를 입력해주세요."
+        }
+    };
+    handleSubmit = (e) => {
+        console.log('submitted');
+        this.props.submit(this.state.userInput);
+        this.state.userInput = '';
+        e.preventDefault();
+    };
+    handleChange = (event) => {
+        console.log('userInput is ' + this.state.userInput);
+        this.setState({
+            userInput: event.target.value
+        })
+    };
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="input-field">
+                        <input
+                            type='text'
+                            value={this.state.userInput}
+                            onChange={this.handleChange}
+                            placeholder={this.state.userInputPlaceHolder}
+                        />
+                    </div>
+                    <input
+                        type='submit'
+                        value='Submit'
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Writing;
